176155312e84f3e9331a341428fd87ff76f8e937
kazuhide1272 <im0176sp@ed.ritsumei.ac.jp>
2020-09-15 20:05:25 +0900

    <commit message>

---
+++ Assets/Miyamoto/Scripts/KeyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KeyMovement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+}
